82bf8e54d565dcf6670107d3d0fd0e53fa50c5db
jak.u.b.p <130249082+uba1709@users.noreply.github.com>
2026-05-09 21:40:36 +0200

    02

--- main.cpp
+++ main.cpp
@@ -7,6 +7,20 @@ using namespace std;
 
 
 int main(){
+    int stopien;
+    cout << "Podaj stopien wielomianu: ";
+    cin >> stopien;
 
+    // Deklaracja wektora o rozmiarze stopien + 1
+    // (bo wielomian stopnia n ma n+1 wspolczynnikow)
+    vector<double> wielomian(stopien + 1);
+
+    cout << "Podaj wspolczynniki (od najwyzszej potegi):" << endl;
+
+    // Wypelnianie wektora
+    for (int i = stopien; i >= 0; i--) {
+        cout << "Wspolczynnik przy x^" << i << ": ";
+        cin >> wielomian[i];
+    }
     return 0;
 }
